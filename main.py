8ca6cd718811f0bcc349c767f3f36fe17963fabc
Atharv Joshi <joshiatharv1078@protonmail.com>
2021-09-29 20:31:15 +0530

    basic get req, path and query params

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/blogs')
+def index(limit: Optional[int] = 10, published: Optional[bool] = True):
+    if(published):
+        return f'{limit} are published'
+    else:
+        return f'{limit} are unpublished'
+
+
+@app.get('/blogs/unpublished')
 def index():
-    return {"data": 'hello world'}
+    return {'data': 'unpublished blogs'}
 
 
-@app.get('/about')
-def about():
-    return 'about page'
+@app.get('/blogs/{id}')
+def about(id: int):
+    return{"data": id}
